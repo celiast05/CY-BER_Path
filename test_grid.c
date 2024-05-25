e431224345ab1b3c539fe17d24111100edd9dace
charmelle5 <nanacharmelle77@gmail.com>
2024-05-26 00:27:19 +0200

    Update test_grid.c

--- test_grid.c
+++ test_grid.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+#include "game.h"
+#include <time.h>
+#include <stdlib.h>
 
+
+
+int main(int argc, char const *argv[]) {
+    srand(time(NULL));
+    int width = rand() % (MAX - MIN + 1) + MIN; // largeur
+    int height = rand() % (MAX - MIN + 1) + MIN; // hauteur
+    int nb_players, test_nb_players; 
+
+    Grid grid = createGridEmpty(width, height); //CreerGrilleVide
+    //grille.cases[1][4] = creeCase(CIBLE, 1, true, true);
+    Coordinate coord;
+    placeTarget(grid, coord);
+    placeRobot(grid, coord);
+    placeWalls(grid);
+    displayGrid(grid);
+  int duration;
+  int difficult;
+
+  do{
+        printf("Combien de joueur joue ?\n");
+        scanf("%d",&nb_players);
+  }while(nb_players < 2 || nb_players > 4);
+    printf("Choisissez un niveau de difficulté (1: facile, 2: moyen, 3: difficile): ");
+    scanf("%d", &difficult);
+    
+    
+  TimeDisplacement(grid, duration , nb_players);
+    
+
+      for (int i = 0; i < grid.width; i++) {
+        free(grid.cases[i]);
+
+     }
+    free(grid.cases);
+    return 0;
+}
+
+  
