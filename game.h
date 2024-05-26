654bcc9b9d832252c911f96c4ebd0b87bfb31a3d
celiast05 <169034880+celiast05@users.noreply.github.com>
2024-05-26 22:19:58 +0200

    Update game.h

--- game.h
+++ game.h
@@ -4,6 +4,7 @@
 
 #define MAX 20
 #define MIN 15
+#define MAXHALF 5
 #define couleur(param) printf("\033[%sm",param)
 
 typedef enum {
@@ -37,28 +38,25 @@ typedef struct {
     int y;
 } Coordinate;
 
-
-void vide_file();
-
 Case createCase (TypeCase type, int id,bool wallRight, bool wallBottom);
 Grid createGridEmpty (int width, int height);
-
+Coordinate createCoordinate(int x, int y);
 void displayCase (Case case1);
 void displayGrid (Grid gridGame);
-
-Coordinate createCoordinate(int x, int y);
-Case getCase(Grid grid, Coordinate coords);
-
-bool canMove (Grid grille, Coordinate coords, Direction direction);
 bool validePlacementTarget(Grid gridGame, Coordinate target);
-void placeTarget (Grid gridGame, Coordinate coord);
+void placeTarget (Grid gridGame, Coordinate targetCoord[]);
 bool validePlacementRobot(Grid gridGame, Coordinate robot);
-void placeRobot(Grid gridGame, Coordinate coord);
-void placerWall (Grid gridGame);
+void placeRobot(Grid gridGame, Coordinate robotCoord[]);
+void placeWall (Grid gridGame);
 
-Direction chooseDirection (char touche);
-void incrementationRobot (Grid gridGame, Coordinate robot, Direction mouvement);
-int actifPlayer (int *mouvement, int player);
-void timeDisplacement(Grid gridGame, int difficultLevel, int player, int *mouvement);
+void vide_file();
+Case getCase(Grid grid, Coordinate coords);
+bool canMove (Grid grille, Coordinate coords, Direction direction);
+char getDirection();
+Direction chooseDirection (char input);
+int timeReflexion(int difficultLevel);
+int actifPlayer (int *movement, int player);
+void movementRobot(Grid gridGame, Direction direction, Coordinate robot, int *moveCount);
+void copyGrid (Grid gridGame, Grid copy);
 
 #endif
